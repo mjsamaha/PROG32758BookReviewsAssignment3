0abab7cac076da47bef85e29c0cf4a31e5cb48d6
Matthew <matthewsamaha@gmail.com>
2025-04-04 19:18:01 -0400

    index page modifications

--- PROG32758BookReviewsGroup20Assignment3/src/main/java/controllers/BookController.java
+++ PROG32758BookReviewsGroup20Assignment3/src/main/java/controllers/BookController.java
@@ -1,7 +1,14 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
+
+    // map to index
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
