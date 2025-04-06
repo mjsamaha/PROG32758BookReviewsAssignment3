3f6bf203d7787832a0bda88000be7d8d31acad51
Matthew <matthewsamaha@gmail.com>
2025-04-06 17:26:45 -0400

    cleanup of code

--- PROG32758BookReviewsGroup20Assignment3/src/main/java/controllers/ReviewsController.java
+++ PROG32758BookReviewsGroup20Assignment3/src/main/java/controllers/ReviewsController.java
@@ -5,14 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ReviewsController {
 
-    /*
-        RequestMapping("/addReview")
-        .addReview
 
-        Mapping to add a review based on Book ID...
-
-        return back to index
-     */
 
 
 }
